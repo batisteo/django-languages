d6c2990a94f8dbd2fd4e0b3d690bf2d1676c099b
Ander Mart?nez <ander.basaundi@gmail.com>
2012-10-28 20:39:22 +0100

    Get the length for the field right. 3 should be enough

--- django_languages/fields.py
+++ django_languages/fields.py
@@ -5,4 +5,5 @@ class LanguageField(CharField):
     A language field for Django models.
     """
     def __init__(self, *args, **kwargs):
+        kwargs.setdefault('max_length', 3)
         super(CharField, self).__init__(*args, **kwargs)
